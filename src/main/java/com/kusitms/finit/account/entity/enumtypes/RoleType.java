fe4913e7449f4eaf6fe04f30a1268aed5d2c9e13
yujeongkimm <20201111@duksung.ac.kr>
2022-11-04 17:37:42 +0900

    FEAT : Spring Security 기능 추가

---
+++ src/main/java/com/kusitms/finit/account/entity/enumtypes/RoleType.java
@@ -0,0 +1,5 @@
+package com.kusitms.finit.account.entity.enumtypes;
+
+public enum RoleType {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/com/kusitms/finit/configure/security/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.kusitms.finit.configure.security;
+
+import com.kusitms.finit.configure.security.jwt.JwtAuthenticationFilter;
+import com.kusitms.finit.configure.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests()
+                .antMatchers("/login/**").permitAll()
+                //.antMatchers("/serialNumber", "/order").hasRole("USER")
+                .and()
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+
+
+    }
+
+}
