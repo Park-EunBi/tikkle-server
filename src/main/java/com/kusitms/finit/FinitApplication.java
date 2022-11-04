182637c296e353983c868043a98939ed73c2866e
yujeongkimm <20201111@duksung.ac.kr>
2022-11-04 15:42:49 +0900

    CHORE : 프로젝트 초기파일 구성

---
+++ src/main/java/com/kusitms/finit/FinitApplication.java
@@ -0,0 +1,13 @@
+package com.kusitms.finit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinitApplication.class, args);
+	}
+
+}
