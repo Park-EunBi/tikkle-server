353554b80998116421b5a399215bd16ba722e92f
yujeongkimm <20201111@duksung.ac.kr>
2022-11-07 20:24:21 +0900

    FEAT : oAuth 카카오 로그인 기능 개발

--- src/main/java/com/kusitms/finit/configure/response/exception/CustomExceptionStatus.java
+++ src/main/java/com/kusitms/finit/configure/response/exception/CustomExceptionStatus.java
@@ -11,7 +11,8 @@ public enum CustomExceptionStatus {
 
     EMPTY_JWT(false, 2001, "JWT를 입력해주세요."),
     INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
-    NOT_AUTHENTICATED_ACCOUNT(false, 2004, "로그인이 필요합니다.");
+    NOT_AUTHENTICATED_ACCOUNT(false, 2004, "로그인이 필요합니다."),
+    OAUTH_EMPTY_INFORM(false, 2560, "OAuth에 필요한 정보가 누락되었습니다."),;
 
     private final boolean isSuccess;
     private final int code;
