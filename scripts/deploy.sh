ec095939bada86ea00c077a3b17e0d8236d870d4
김유정 <uuuuujeong00000@naver.com>
2022-11-18 16:59:03 +0900

    CI : deploy script 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,7 +20,12 @@ else
 fi
 
 echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls $REPOSITORY/ | grep 'finit' | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME에 실행권한 추가"
+chmod +x $JAR_NAME
+
+echo ">$JAR_NAME 실행"
 nohup java -jar $REPOSITORY/$JAR_NAME &
