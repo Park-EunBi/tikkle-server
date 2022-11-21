538bfdd5ab41782807fb46f53a35f0612894c9d9
김유정 <uuuuujeong00000@naver.com>
2022-11-21 22:32:14 +0900

    FIX : 세부 챌린지 신청 - 응답형태 수정

--- src/main/java/com/kusitms/finit/participation/ParticipationController.java
+++ src/main/java/com/kusitms/finit/participation/ParticipationController.java
@@ -1,5 +1,7 @@
 package com.kusitms.finit.participation;
 
+import com.kusitms.finit.configure.response.CommonResponse;
+import com.kusitms.finit.configure.response.ResponseService;
 import com.kusitms.finit.configure.security.authentication.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ParticipationController {
 
     private final ParticipationService participationService;
+    private final ResponseService responseService;
 
     // 세부 챌린지 참여
     @PostMapping("/participate/{challengeDetailId}")
-    public void participateByChallengeId(@AuthenticationPrincipal CustomUserDetails customUserDetails,
-                                         @PathVariable(value = "challengeDetailId") Long id) {
+    public CommonResponse participateByChallengeId(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                                   @PathVariable(value = "challengeDetailId") Long id) {
         participationService.participateByChallengeId(customUserDetails, id);
+        return responseService.getSuccessResponse();
     }
 }
