2313eb4792d1932fc9d086c313e6f32848b18c2e
김유정 <uuuuujeong00000@naver.com>
2022-11-24 15:00:27 +0900

    FIX : 세부 챌린지 조회 반환값 추가

--- src/main/java/com/kusitms/finit/challengeDetail/dto/ChallengeDetailResultRes.java
+++ src/main/java/com/kusitms/finit/challengeDetail/dto/ChallengeDetailResultRes.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 @Data
 public class ChallengeDetailResultRes {
+    private String writer;
     private String title;
     private String intro;
     private String target;
@@ -12,10 +13,11 @@ public class ChallengeDetailResultRes {
     private int feed;
 
     public ChallengeDetailResultRes(ChallengeDetail challengeDetail, int feed) {
-        title = challengeDetail.getTitle();
-        intro = challengeDetail.getIntro();
-        target = challengeDetail.getTarget();
-        participation = challengeDetail.getParticipate();
-        feed = feed;
+        this.writer = challengeDetail.getAccount().getNickname();
+        this.title = challengeDetail.getTitle();
+        this.intro = challengeDetail.getIntro();
+        this.target = challengeDetail.getTarget();
+        this.participation = challengeDetail.getParticipate();
+        this.feed = feed;
     }
 }
