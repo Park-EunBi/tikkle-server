6a850a97f59c91362ce917cf57c2b72314ccdf01
김유정 <uuuuujeong00000@naver.com>
2022-11-24 14:55:43 +0900

    FIX : 세부 챌린지 전체 조회 반환값 추가

--- src/main/java/com/kusitms/finit/challengeDetail/dto/ChallengeDetailRes.java
+++ src/main/java/com/kusitms/finit/challengeDetail/dto/ChallengeDetailRes.java
@@ -5,11 +5,15 @@ import lombok.Data;
 
 @Data
 public class ChallengeDetailRes {
+    private Long id;
     private String title;
     private String intro;
+    private int participate;
 
     public ChallengeDetailRes(ChallengeDetail challengeDetail) {
+        id = challengeDetail.getId();
         title = challengeDetail.getTitle();
         intro = challengeDetail.getIntro();
+        participate = challengeDetail.getParticipate();
     }
 }
